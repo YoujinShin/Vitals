44c455704aab6ad77135efd1068955e975b30cf5
Youjin Shin <meggonagul@gmail.com>
2014-09-05 16:34:49 -0400

    add progress bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
-
+// $(function(){
+// 	window.randomize = function() {
+// 	  $('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
+// 	}
+// 	setTimeout(window.randomize, 200);
+// 	$('.radial-progress').click(window.randomize);
+// });
